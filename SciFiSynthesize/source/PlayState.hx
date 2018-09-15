e99a0dd875372161e88bf9a93fe518f57294a0ed
Colin <cehiggins0@gmail.com>
2018-09-15 14:57:31 -0400

    Added Basic Player Movement
    Player can walk left, right, and jump.
    Gravity is present when jumping.
    Collisions are detected with ground.

--- SciFiSynthesize/source/PlayState.hx
+++ SciFiSynthesize/source/PlayState.hx
@@ -1,16 +1,35 @@
 package;
 
 import flixel.FlxState;
-
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import flixel.FlxG;
 class PlayState extends FlxState
 {
+	var _player:Player;
+	var _ground:FlxSprite;
 	override public function create():Void
 	{
+		_player = new Player(320, 200);
+		add(_player);
+		_ground = new FlxSprite();
+		_ground.makeGraphic(640,240,FlxColor.GRAY);
+		_ground.x = 0;
+		_ground.y = 240;
+		add(_ground);
 		super.create();
 	}
 
+
 	override public function update(elapsed:Float):Void
 	{
+		if (FlxG.collide(_player,_ground)){
+			_player.grounded();
+			_ground.velocity.set(0,0);
+			_ground.x=0;
+			_ground.y= 240;
+		}
 		super.update(elapsed);
 	}
 }
---
+++ SciFiSynthesize/source/Player.hx
@@ -0,0 +1,84 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+
+class Player extends FlxSprite  {
+  public  var speed:Float = 100;
+  public  var jump:Float = 200;
+  public  var xvel:Float = 0; //store x velocity
+  public  var yvel:Float = 0; // store y velocity
+  public  var airborne:Bool = true; //is the player off the ground?
+  public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) {
+    super(X,Y,SimpleGraphic);
+    makeGraphic(20, 20, FlxColor.BLUE);
+    drag.x = 1000;
+    drag.y = 0;
+  }
+  function move():Void {
+    var _up:Bool = false;
+    var _down:Bool = false;
+    var _left:Bool = false;
+    var _right:Bool = false;
+    var _oldx:Float = xvel;
+    var _oldy:Float = yvel;
+    _up = FlxG.keys.anyPressed([UP, W]);
+    _down = FlxG.keys.anyPressed([DOWN, S]);
+    _left = FlxG.keys.anyPressed([LEFT, A]);
+    _right = FlxG.keys.anyPressed([RIGHT, D]);
+    if (airborne){
+      _oldy = _oldy+4;
+      velocity.set(_oldx,_oldy);
+      yvel = _oldy;
+      trace( velocity);
+
+    }
+    else if (_up && _down){
+      _up = _down = false;
+    }
+    else if (_left && _right){
+      _left = _right = false;
+    }
+    else if (_left && _up){
+      velocity.set(-speed,-jump);
+      xvel=-speed;
+      yvel = -jump;
+      airborne = true;
+    }
+    else if (_right && _up){
+      velocity.set(speed,-jump);
+      xvel=speed;
+      yvel = -jump;
+      airborne = true;
+    }
+    else if (_left ){
+      velocity.set(-speed,0);
+      xvel=-speed;
+    }
+    else if (_right){
+      velocity.set(speed, 0);
+      xvel = speed;
+    }
+    else if(_up){
+      velocity.set(0,-jump);
+      airborne = true;
+      yvel = -jump;
+
+    }
+  }
+
+  public function grounded() : Bool {
+    airborne = false;
+    velocity.set(xvel,0);
+    yvel = 0;
+    return true;
+  }
+
+  override public function update( elapsed:Float ) : Void {
+    move();
+    super.update(elapsed);
+  }
+
+}
