9e7e89471d27bd12bf8dccf2c8723332f57141c0
Colin <cehiggins0@gmail.com>
2018-09-19 15:52:39 -0400

    Fixed typo on title screen

--- SciFiSynthesize/source/MainMenuState.hx
+++ SciFiSynthesize/source/MainMenuState.hx
@@ -9,24 +9,24 @@ class MainMenuState extends FlxState
 {
 	var _startButton:FlxButton;
 	var _titleText:FlxText;
-	
+
 	override public function create(): Void {
 		_startButton = new FlxButton(10, 10, "Start", start);
 		_startButton.screenCenter();
 		add(_startButton);
-		
-		_titleText = new FlxText(100, 100, 300, "Sythesize", 50);
+
+		_titleText = new FlxText(100, 100, 350, "Synthesize", 50);
 		_titleText.screenCenter();
 		_titleText.y -= 50;
 		add(_titleText);
-		
+
 		super.create();
 	}
-	
+
 	override public function update(elapsed:Float): Void {
 		super.update(elapsed);
 	}
-	
+
 	function start(): Void {
 		FlxG.switchState(new PlayState());
 	}
