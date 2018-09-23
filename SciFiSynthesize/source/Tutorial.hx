ae3f554ad1e130090486097567d0aa68ff08093b
Eric Franco <france2@rpi.edu>
2018-09-23 13:47:16 -0400

    Background Loaded In
    Background added, not sure what collides yet

--- SciFiSynthesize/source/Tutorial.hx
+++ SciFiSynthesize/source/Tutorial.hx
@@ -13,9 +13,19 @@ class Tutorial extends FlxState { //we can have this extend PlayState later
     var _player:Player;
     var _map:TiledMap;
     var _mWalls:FlxTilemap;
+    var _mBackground:FlxTilemap;
 
     override public function create():Void {
         _map = new TiledMap(AssetPaths.tutorial__tmx);
+
+        _mBackground = new FlxTilemap();
+        _mBackground.loadMapFromArray(cast(_map.getLayer("Background"), TiledTileLayer).tileArray, _map.width, _map.height,
+            AssetPaths.labset__png, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
+        _mBackground.follow();
+        _mBackground.setTileProperties(2, FlxObject.NONE);
+        _mBackground.setTileProperties(3, FlxObject.ANY);
+        add(_mBackground);
+
         _mWalls = new FlxTilemap();
         _mWalls.loadMapFromArray(cast(_map.getLayer("Ground"), TiledTileLayer).tileArray, _map.width, _map.height,
              AssetPaths.labset__png, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
@@ -23,6 +33,8 @@ class Tutorial extends FlxState { //we can have this extend PlayState later
         _mWalls.setTileProperties(2, FlxObject.NONE);
         _mWalls.setTileProperties(3, FlxObject.ANY);
         add(_mWalls);
+
+        
 
         _player = new Player(20, 20);
         //var tmpMap:TiledObjectLayer = cast _map.getLayer("entities");
