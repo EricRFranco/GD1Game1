c6982cd0aa0b21b46387f698c9422b91ac44e6ab
Eric Franco <france2@rpi.edu>
2018-09-23 20:05:40 -0400

    All objects imported

--- SciFiSynthesize/source/Tutorial.hx
+++ SciFiSynthesize/source/Tutorial.hx
@@ -15,6 +15,8 @@ class Tutorial extends FlxState { //we can have this extend PlayState later
     var _mWalls:FlxTilemap;
     var _mBackground:FlxTilemap;
     var _mDecorations:FlxTilemap;
+    var _mBoxes:FlxTilemap;
+    var _mComputers:FlxTilemap;
 
     override public function create():Void {
         _map = new TiledMap(AssetPaths.tutorial__tmx);
@@ -34,6 +36,22 @@ class Tutorial extends FlxState { //we can have this extend PlayState later
         _mDecorations.setTileProperties(2, FlxObject.NONE);
         _mDecorations.setTileProperties(3, FlxObject.ANY);
         add(_mDecorations);
+
+        _mBoxes = new FlxTilemap();
+        _mBoxes.loadMapFromArray(cast(_map.getLayer("Boxes"), TiledTileLayer).tileArray, _map.width, _map.height,
+            AssetPaths.labset__png, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
+        _mBoxes.follow();
+        _mBoxes.setTileProperties(2, FlxObject.NONE);
+        _mBoxes.setTileProperties(3, FlxObject.ANY);
+        add(_mBoxes);
+
+        _mComputers = new FlxTilemap();
+        _mComputers.loadMapFromArray(cast(_map.getLayer("Computer"), TiledTileLayer).tileArray, _map.width, _map.height,
+            AssetPaths.labset__png, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
+        _mComputers.follow();
+        _mComputers.setTileProperties(2, FlxObject.NONE);
+        _mComputers.setTileProperties(3, FlxObject.ANY);
+        add(_mComputers);
 
         _mWalls = new FlxTilemap();
         _mWalls.loadMapFromArray(cast(_map.getLayer("Ground"), TiledTileLayer).tileArray, _map.width, _map.height,
