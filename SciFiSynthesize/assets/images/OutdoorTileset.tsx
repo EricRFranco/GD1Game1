9519beaf636143ad46c421cfe8ae26d16303c1ae
Eric Franco <france2@rpi.edu>
2018-09-26 06:03:24 -0400

    level 2 tilemap

---
+++ SciFiSynthesize/assets/images/OutdoorTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="OutdoorTileset" tilewidth="14" tileheight="14" tilecount="196" columns="14">
+ <image source="OutdoorTileset.png" width="200" height="200"/>
+</tileset>
