6f5388ee84124e3012335571b41c31eb98d4581f
Colin <cehiggins0@gmail.com>
2018-09-26 02:04:14 -0400

    Movable Boxes can now fall and will kill enemies they land on

--- SciFiSynthesize/source/Box.hx
+++ SciFiSynthesize/source/Box.hx
@@ -6,16 +6,27 @@ import flixel.util.FlxColor;
 import flixel.FlxG;
 
 class Box extends FlxSprite {
-
+    public var airborne:Bool = false;
     public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) {
         super(X,Y,SimpleGraphic);
         makeGraphic(50,50,FlxColor.GREEN);
         drag.x = 1000;
         drag.y = 1000;
-        allowCollisions = 0x0011;
+        allowCollisions = 0x1111;
     }
 
     override public function update(elapsed:Float) {
         super.update(elapsed);
+        if(airborne){
+          acceleration.set(0,25);
+        }
+    }
+
+    public function grounded() : Bool {
+      trace("grounding");
+      airborne = false;
+      velocity.set(0, 0);
+      acceleration.set(0,0);
+      return true;
     }
 }
