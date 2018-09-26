c0a79b3cbfe33d2d1e6143ed00649d70612bdcb0
Eric Franco <france2@rpi.edu>
2018-09-26 02:38:39 -0400

    Creation of LevelOne.hx

---
+++ SciFiSynthesize/source/LevelOne.hx
@@ -0,0 +1,25 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.tiled.TiledMap;
+import flixel.addons.editors.tiled.TiledTileLayer;
+import flixel.tile.FlxBaseTilemap;
+import flixel.FlxObject;
+import flixel.FlxG;
+import flixel.FlxCamera;
+import flixel.util.FlxColor;
+import flixel.group.FlxGroup;
+import flixel.addons.display.FlxBackdrop;
+import flixel.text.FlxText;
+import sys.io.File;
+
+class LevelOne extends PlayState {
+
+    var _map:TiledMap;
+
+    public override function create() : Void {
+
+    }
+}
