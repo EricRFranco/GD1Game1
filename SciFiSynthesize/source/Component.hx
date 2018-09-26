53a54d5be4058d73d6a0f4af5ab42c2f918a2fb2
Corey D <coreyd2108@gmail.com>
2018-09-25 20:33:42 -0400

    Add graphics for components

--- SciFiSynthesize/source/Component.hx
+++ SciFiSynthesize/source/Component.hx
@@ -11,7 +11,25 @@ class Component extends FlxSprite {
     public function new(s:String, ?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) {
         super(X,Y,SimpleGraphic);
         _label = s;
-        makeGraphic(20, 20, FlxColor.RED);
+		
+		switch(s) {
+			case("Antennae"):
+				loadGraphic("assets/images/antennae.png");
+			case("Battery"):
+				loadGraphic("assets/images/battery.png");
+			case("Dumbell"):
+				loadGraphic("assets/images/dumbell.png");
+			case("Fan"):
+				loadGraphic("assets/images/fan.png");
+			case("Glove"):
+				loadGraphic("assets/images/glove.png");
+			case("Microphone"):
+				loadGraphic("assets/images/microphone.png");
+			case("Shoe"):
+				loadGraphic("assets/images/shoe.png");
+			case("Spring"):
+				loadGraphic("assets/images/spring.png");
+		}
     }
 
     public function getLabel():String {
