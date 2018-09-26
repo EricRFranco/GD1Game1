68bd8f25c2ae80ab29e9353d961762197e19738c
Eric Franco <france2@rpi.edu>
2018-09-26 07:54:07 -0400

    SwatShield Attack Animation

--- SciFiSynthesize/source/Enemy.hx
+++ SciFiSynthesize/source/Enemy.hx
@@ -39,7 +39,8 @@ class Enemy extends FlxSprite {
       y+=27;
       loadGraphic("assets/images/SwatShieldMoving.png", true, 100,100); //  54 X 73 image
       animation.add("Walk", [0,1,2,3,4,5,6,7], 15);
-      animation.play("Walk");
+      animation.add("Attack", [7,8], 15);
+      //animation.play("Attack"); //For debug
       health = 2;
       if (R == 0){
         patrolLeft = x-(400);
